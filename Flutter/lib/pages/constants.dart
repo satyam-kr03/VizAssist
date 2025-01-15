7e9ec7c644e0ef93a7d74259ccf36c1d656d94d1
Satyam Kumar <b22236@students.iitmandi.ac.in>
2025-01-15 20:13:00 +0100

    url update

--- Flutter/lib/pages/constants.dart
+++ Flutter/lib/pages/constants.dart
@@ -1,2 +1,2 @@
-const String CAPTION_API_URL = "https://bd82-35-230-19-219.ngrok-free.app/caption";
-const String CHAT_API_URL = "https://bd82-35-230-19-219.ngrok-free.app/chat";
+const String CAPTION_API_URL = "https://3f2d-138-246-3-127.ngrok-free.app/caption";
+const String CHAT_API_URL = "https://3f2d-138-246-3-127.ngrok-free.app/chat";
